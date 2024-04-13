bfec71ce8c512ef542714e65236d59ceb40229ef
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-04-13 19:34:54 +0300

    Update TodoList.js

--- src/my_component/seminar_3/DZ/TodoList.js
+++ src/my_component/seminar_3/DZ/TodoList.js
@@ -3,52 +3,63 @@ import {
   TextField,
   Button,
   Card,
+  IconButton,
   List,
   ListItem,
   ListItemText,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const TodoList = () => {
+function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  const handleAddTask = () => {
+  const addTask = () => {
     if (newTask.trim() !== "") {
       setTasks([...tasks, newTask]);
       setNewTask("");
     }
   };
 
-  const handleDeleteTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((a, i) => i !== index);
     setTasks(updatedTasks);
   };
 
   return (
     <div>
+      <h2>Список дел</h2>
       <TextField
         label="Введите новую задачу"
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
+        style={{ margin: 20 }}
       />
-      <Button variant="contained" onClick={handleAddTask}>
+      <Button
+        variant="contained"
+        onClick={addTask}
+        style={{ marginTop: 30, marginLeft: 20 }}
+      >
         Добавить задачу
       </Button>
 
       <List>
         {tasks.map((task, index) => (
           <ListItem key={index}>
-            <Card>
-              <ListItemText primary={task} />
-              <Button onClick={() => handleDeleteTask(index)}>
-                <Button />
-              </Button>
+            <Card
+              variant="outlined"
+              style={{ minWidth: 350, display: "flex", padding: 15 }}
+            >
+              <ListItemText primary={task} s />
+              <IconButton onClick={() => deleteTask(index)}>
+                <DeleteIcon variant="contained" size="small" />
+              </IconButton>
             </Card>
           </ListItem>
         ))}
       </List>
     </div>
   );
-};
+}
 
 export default TodoList;
