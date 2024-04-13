c82fb3659649876e7183d3813421e64ea285d549
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-04-13 19:48:57 +0300

    Добавлен App.js

---
+++ src/my_component/seminar_3/DZ/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TemperatureConverter from "./my_component/seminar_3/DZ/TemperatureConverter";
+import TodoList from "./my_component/seminar_3/DZ/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TemperatureConverter />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
